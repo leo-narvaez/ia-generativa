e2cc8dfce4f297c274d34ace6c11260b84e95de2
leo-narvaez <leonardo.narvaez@tajamar365.com>
2025-01-31 10:59:23 +0100

    Ms Learn AI Services

---
+++ mslearn-ai-services/03-monitor-ai-services/rest-test.cmd
@@ -0,0 +1,1 @@
+curl -X POST "https://az-services-leo.cognitiveservices.azure.com//language/:analyze-text?api-version=2023-04-01" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 6AGNvzeKEEDWiTtmR9267BUrRxukWOvPoGybGh9VQejwZTXIgMF5JQQJ99BAACYeBjFXJ3w3AAAEACOGQHAD" --data-ascii "{'analysisInput':{'documents':[{'id':1,'text':'hello'}]}, 'kind': 'LanguageDetection'}"
---
+++ mslearn-ai-services/03-monitor-ai-services/rest-test.sh
@@ -0,0 +1,1 @@
+curl -X POST "https://az-services-leo.cognitiveservices.azure.com//text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key:6AGNvzeKEEDWiTtmR9267BUrRxukWOvPoGybGh9VQejwZTXIgMF5JQQJ99BAACYeBjFXJ3w3AAAEACOGQHAD" --data-ascii "{'documents':           [{'id':1,'text':'hello'}]}"
